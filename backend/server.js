42150dfc32e6bd668a88b32256a2fe51674eaf52
TAHANG OPARA YVAN DIDIER <yvanlandry444@gmail.com>
2025-06-23 02:14:48 +0200

    fdfdf

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,10 @@ connectDB();
 connectCloudinary();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+  origin: '*',
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
