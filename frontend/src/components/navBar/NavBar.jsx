8021a83046c3b1610757badfeadf7bf3ad9a636b
TAHANG OPARA YVAN DIDIER <yvanlandry444@gmail.com>
2025-06-26 22:50:37 +0200

    fdfdfd

--- frontend/src/components/navBar/NavBar.jsx
+++ frontend/src/components/navBar/NavBar.jsx
@@ -1,17 +1,19 @@
 import React, { useContext } from 'react';
 import './navBar.css';
 import { NavLink, useNavigate } from "react-router-dom";
+
 import { FaHome } from "react-icons/fa";
+import { Home, LayoutDashboard, Landmark, MapPin } from "lucide-react";
 import { assets } from '../../assets/assets';
 import { ShopContext } from '../../context/shopContext';
 
 export default function NavBar() {
   const { getCartCount, token } = useContext(ShopContext);
-  const navigate = useNavigate(); // hook pour naviguer
+  const navigate = useNavigate();
 
   const handleProfileClick = (e) => {
     if (!token) {
-      e.preventDefault(); // empêche la navigation normale
+      e.preventDefault();
       navigate('/login');
     }
   };
@@ -20,24 +22,36 @@ export default function NavBar() {
     <nav className="bottom-nav">
       <ul>
         <li>
-          <NavLink to="/" className="nav-link">
-            <FaHome className="icon" />
+          <NavLink 
+            to="/" 
+            className={({isActive}) => isActive ? "nav-link active" : "nav-link"}
+          >
+            <Home className="icon" />
             <span>Accueil</span>
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="/cart" className="nav-link">
-            <img className="cart-icon" src={assets.cart_icon} alt='' />
-            {getCartCount() > 0 && (
-              <p className="cart-count">{getCartCount()}</p>
-            )}
+          <NavLink 
+            to="/cart" 
+            className={({isActive}) => isActive ? "nav-link active" : "nav-link"}
+          >
+            <div className="icon-container">
+              <img className="cart-icon" src={assets.cart_icon} alt='' />
+              {getCartCount() > 0 && (
+                <p className="cart-count">{getCartCount()}</p>
+              )}
+            </div>
             <span>Panier</span>
           </NavLink>
         </li>
 
         <li>
-          <NavLink to="/profil" className="nav-link" onClick={handleProfileClick}>
+          <NavLink 
+            to="/profil" 
+            className={({isActive}) => isActive ? "nav-link active" : "nav-link"}
+            onClick={handleProfileClick}
+          >
             <img className="profile-icon" src={assets.profile_icon} alt="" />
             <span>Profil</span>
           </NavLink>
